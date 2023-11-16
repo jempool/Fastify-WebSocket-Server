db0fd3b298792ed0828bc62a4d0a750a6fa9092c
Jem Suarez <jempoolsmz@gmail.com>
2023-11-16 11:07:56 -0500

    Addinng webSockets feature

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 
 const path = require('node:path')
 const AutoLoad = require('@fastify/autoload')
+const fastifySocketIo = require('fastify-socket.io')
+
+const socketIO = require('./src/webSockets/webSockets.js');
 
 // Pass --options via CLI arguments in command to enable these options.
 const options = {}
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+
+  // === WebSockets ===
+  await fastify.register(fastifySocketIo);
+  socketIO(fastify);
 
   // Do not touch the following lines
 
