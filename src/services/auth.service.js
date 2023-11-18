46def7be0326372d147b13e44c9c2c988a25eb22
Jem Suarez <jempoolsmz@gmail.com>
2023-11-17 21:07:06 -0500

    Adding Login and Signup APIs

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,6 +3,7 @@
 const dbService = require('../services/db.service.js');
 const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 
 module.exports = {
   login: function (request, reply, done) {
@@ -19,6 +20,32 @@ module.exports = {
       .catch((err) => {
         console.error(err);
         done(new Error('failure to communicate with the database.'));
+      });
+  },
+  signup: function (request, reply, done) {
+    const email = request.body.email;
+    const name = request.body.name;
+    const password = request.body.password;
+    dbService.getUserByEmail(email)
+      .then((user) => {
+        if (user) {
+          const err = new Error(`The email ${email} is already associated with an account`);
+          err.statusCode = 400;
+          return done(err);
+        }
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
+        dbService.createUser({ name, email, password: hash })
+          .then((user) => {
+            reply.locals.user = user.toResponseObject();
+            return done();
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        const err = new Error('failure to communicate with the database.');
+        err.statusCode = 500;
+        return done(err);
       });
   }
 };
