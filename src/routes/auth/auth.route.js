1831fb6ee06c2f8a7ace81b6cbf3dc875b62a6ce
Jem Suarez <jempoolsmz@gmail.com>
2023-11-27 21:30:22 -0500

    Removing '@fastify/auth' from the Login, Signup and Refresh Token

---
+++ src/routes/auth/auth.route.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fp = require('fastify-plugin');
+
+const authController = require('../../controllers/auth.controller.js');
+const authService = require('../../services/auth.service.js');
+const authSchemas = require('../../schemas/auth.schema.js');
+
+
+module.exports = fp(async function (fastify, opts) {
+
+  /* POST Login. */
+  fastify.route({
+    method: 'POST',
+    url: '/auth/login',
+    schema: authSchemas.loginSchema,
+    preHandler: authService.login,
+    handler: authController.login
+  });
+
+  /* POST Signup. */
+  fastify.route({
+    method: 'POST',
+    url: '/auth/signup',
+    schema: authSchemas.signupSchema,
+    preHandler: authService.signup,
+    handler: authController.login
+  });
+
+  /* POST Refresh token. */
+  fastify.route({
+    method: 'POST',
+    url: '/auth/refresh',
+    schema: authSchemas.refreshTokenSchema,
+    preHandler: authService.refreshToken,
+    handler: authController.refreshToken
+  });
+});
+
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -8,62 +8,43 @@ const dbService = require('../services/db.service.js');
 
 
 module.exports = {
-  login: function (request, reply, done) {
+  login: async function (request, reply) {
     const { email, password } = request.body;
-    dbService.getUserByEmail(email)
-      .then((user) => {
-        if (!user || !bcrypt.compareSync(password, user.password)) {
-          const err = new Error('Incorrect email or password.');
-          err.statusCode = 400;
-          return done(err);
-        }
-        reply.locals.user = user.toResponseObject();
-        return done();
-      })
-      .catch((error) => {
-        console.log(error);
-        const err = new Error('failure trying to fetch the user.');
-        err.statusCode = 500;
-        return done(err);
-      });
+    const user = await dbService.getUserByEmail(email);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      const err = new Error('Incorrect email or password.');
+      err.statusCode = 400;
+      throw err;
+    }
+    reply.locals.user = user.toResponseObject();
   },
 
-  signup: function (request, reply, done) {
+  signup: async function (request, reply) {
     const { email, name, password } = request.body;
-    dbService.getUserByEmail(email)
-      .then((user) => {
-        if (user) {
-          const err = new Error(`The email ${email} is already associated with an account`);
-          err.statusCode = 400;
-          return done(err);
-        }
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hash = bcrypt.hashSync(password, salt);
-        dbService.createUser({ name, email, password: hash })
-          .then((user) => {
-            reply.locals.user = user.toResponseObject();
-            return done();
-          });
-      })
-      .catch((error) => {
-        console.log(error);
-        const err = new Error('failure trying to fetch the user.');
-        err.statusCode = 500;
-        return done(err);
-      });
+    const user = await dbService.getUserByEmail(email);
+    if (user) {
+      const err = new Error(`The email ${email} is already associated with an account`);
+      err.statusCode = 400;
+      throw err;
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    const createdUser = await dbService.createUser({ name, email, password: hash });
+    if (createdUser) {
+      reply.locals.user = createdUser.toResponseObject();
+    }
   },
 
-  refreshToken: function (request, reply, done) {
+  refreshToken: async function (request, reply) {
     const { email, refreshToken } = request.body;
     const token = jwt.verify(refreshToken, process.env.JWT_SECRET);
     const isValid = token.user.email === email;
     if (!isValid) {
       const err = new Error('Invalid token, try login again.');
       err.statusCode = 401;
-      return done(err);
+      throw err;
     }
     reply.locals.user = { name: token.user.name, email: token.user.email };
-    return done();
   }
 };
 
